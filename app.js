c91c5ae0bcff745353c1108ee4a22593a58cb005
Thomas Lam <thomaslam2142@gmail.com>
2023-09-29 19:46:54 -0400

    follow me

--- app.js
+++ app.js
@@ -2,13 +2,17 @@ const app = Vue.createApp({
     data(){
         return{
             showBooks:true,
-            title:'The Final Empire',
-            author: 'Brandon Sanderson',
-            age: 45,
+            books:[
+                {title:'name of the wind', author:'patrick rothfuss'},
+                {title:'the way of kings', author:'brandon sanderson'},
+                {title:'the final empire', author:'brandon sanderson'},
+            ],
+            url: "http://www.thenetninja.co.uk",
             x:0,
             y:0
         }
-    },
+        }
+    ,
     methods:{
         toggleShowBooks(){
             console.log('toggleShowBooks')
